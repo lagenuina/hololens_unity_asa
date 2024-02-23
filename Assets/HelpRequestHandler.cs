309967f9d667baa1716cc70dad9e991b27422cdb
Lorena Genua <lgenua@wpi.edu>
2024-02-23 18:46:22 -0500

    Updated for IROS 2024

--- Assets/HelpRequestHandler.cs
+++ Assets/HelpRequestHandler.cs
@@ -48,15 +48,14 @@ public class HelpRequestHandler : MonoBehaviour
     void Start()
     {
         medicineName = "None";
-        ROSConnection.GetOrCreateInstance().RegisterPublisher<StringMsg>("/debug");
+
+        // Subscribers
         ROSConnection.GetOrCreateInstance().Subscribe<PoseMsg>("/my_gen3/target_hologram", TargetUpdate);
-
         ROSConnection.GetOrCreateInstance().Subscribe<TargetInfoMsg>("/target_identifier", TargetIdentifierCallback);
-
-        //ROSConnection.GetOrCreateInstance().Subscribe<StringMsg>("/medicine_name", MedicineNameCallback);
         ROSConnection.GetOrCreateInstance().Subscribe<Int32Msg>("/my_gen3/pick_and_place", RobotStateCallback);
-
         ROSConnection.GetOrCreateInstance().Subscribe<Int32Msg>("/target_counter", TargetCounterCallback);
+
+        // Services
         ROSConnection.GetOrCreateInstance().RegisterRosService<UpdateStateRequest, UpdateStateResponse>("/resume_task");
         ROSConnection.GetOrCreateInstance().ImplementService<UpdateStateRequest, UpdateStateResponse>("/local_help_request_service", RequestHelpCallback);
 
@@ -67,9 +66,51 @@ public class HelpRequestHandler : MonoBehaviour
         tray.SetActive(false);
 
         radialView = TODOMenu.GetComponent<RadialView>();
-        //FollowMeToggle followMeToggle = TODOMenu.GetComponent<FollowMeToggle>();
     }
 
+    // Call service to resume task
+    public void ResumeTask()
+    {
+        CallResumeService(0);
+    }
+
+    public void UpdateTarget()
+    {
+        CallResumeService(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SetActiveObjects(bool targetMedicineActive, bool targetBoxActive, bool trayActive)
+    {
+        targetMedicine.SetActive(targetMedicineActive);
+        targetBox.SetActive(targetBoxActive);
+        tray.SetActive(trayActive);
+    }
+
+    void RobotStateCallback(Int32Msg pickandplaceState)
+    {
+        robotState = pickandplaceState.data;
+    }
+
+    void TargetIdentifierCallback(TargetInfoMsg target)
+    {
+        medicineName = target.name;
+        expirationMonth = target.expiration.Substring(0, 2);
+        expirationYear = target.expiration.Substring(2);
+        markerID = target.id;
+    }
+
+    void TargetCounterCallback(Int32Msg targetCounter)
+    {
+        counter = targetCounter.data;
+    }
+
+    // Converts position from World (HoloLens) to ASA coordinate system or vice versa
     private Vector3 ConvertWorldASA(Vector3 positionVector, string toFrame)
     {
         Vector3 newPositionVector = Vector3.zero; // Initialize the vector
@@ -87,36 +128,21 @@ public class HelpRequestHandler : MonoBehaviour
         return newPositionVector;
     }
 
-    void RobotStateCallback(Int32Msg pickandplaceState)
+    private void UpdateTargetVisibility(Vector3 positionTargetWorld)
     {
-        robotState = pickandplaceState.data;
-    }
+        bool shouldUpdateTray = requestID == 1 || requestID == 2 || requestID == 3;
+        bool isMedicineActive = robotState == 0 || robotState == 1 || robotState == 2 || (requestID != 0 && lhHelp);
+        bool isBoxActive = requestID == 3 && lhHelp;
 
-    void TargetIdentifierCallback(TargetInfoMsg target)
-    {
-        medicineName = target.name;
-        expirationMonth = target.expiration.Substring(0, 2);
-        expirationYear = target.expiration.Substring(2);
-        markerID = target.id;
+        SetActiveObjects(isMedicineActive, isBoxActive, shouldUpdateTray);
 
-        publisher.StringMessage("/debug", expirationMonth);
-    }
+        if (shouldUpdateTray)
+        {
+            UpdateTrayPosition();
+        }
 
-    //void MedicineNameCallback(StringMsg name)
-    //{
-    //    medicineName = name.data;
-
-    //    publisher.StringMessage("/debug", medicineName);
-    //}
-
-    //void MedicineIDCallback(Int32Msg id)
-    //{
-    //    markerID = id.data;
-    //}
-
-    void TargetCounterCallback(Int32Msg targetCounter)
-    {
-        counter = targetCounter.data;
+        targetMedicine.transform.position = positionTargetWorld;
+        targetMedicine.transform.rotation = spatialAnchor.transform.rotation;
     }
 
     void TargetUpdate(PoseMsg targetPose)
@@ -125,92 +151,7 @@ public class HelpRequestHandler : MonoBehaviour
         positionTargetWorld = ConvertWorldASA(positionTargetASA, "ToWorld");
         Quaternion orientationTargetWorld = new Quaternion((float)targetPose.orientation.x, -(float)targetPose.orientation.y, -(float)targetPose.orientation.z, (float)targetPose.orientation.w);
 
-        targetMedicine.transform.position = positionTargetWorld;
-        targetMedicine.transform.rotation = spatialAnchor.transform.rotation;
-
-        if (positionTargetWorld != spatialAnchor.transform.position)
-        {
-            if (requestID != 0 & lhHelp)
-            {
-                if (requestID == 1)
-                {
-                    targetMedicine.SetActive(true);
-                    targetBox.SetActive(false);
-                    UpdateTrayPosition();
-                    tray.SetActive(true);
-                }
-                else if (requestID == 2)
-                {
-                    targetMedicine.SetActive(true);
-                    targetBox.SetActive(false);
-
-                    UpdateTrayPosition();
-                    tray.SetActive(true);
-                }
-                else if (requestID == 3)
-                {
-
-                    targetBox.transform.position = positionTargetWorld;
-                    targetBox.transform.rotation = spatialAnchor.transform.rotation;
-
-                    targetMedicine.SetActive(false);
-
-                    targetBox.SetActive(true);
-
-                    UpdateTrayPosition();
-                    tray.SetActive(true);
-                }
-            }
-            else
-            {
-                if (robotState == 0 || robotState == 1 || robotState == 2)
-                {
-                    targetMedicine.SetActive(true);
-                    tray.SetActive(false);
-                    targetBox.SetActive(false);
-                }
-                else if (robotState == 3 || robotState == 4)
-                {
-                    targetMedicine.SetActive(false);
-                    targetBox.SetActive(false);
-
-                    UpdateTrayPosition();
-
-                    tray.SetActive(true);
-                }
-            }
-            
-        }
-        else
-        {
-            if (requestID == 1)
-            {
-                targetBox.SetActive(false);
-                UpdateTrayPosition();
-                tray.SetActive(true);
-            }
-            targetMedicine.SetActive(false);
-        }
-    }
-
-    Vector3 UpdateTrayPosition()
-    {
-        //if (counter < 3)
-        //{
-        //    positionTray = ConvertWorldASA(new Vector3(0.76f, -0.02f, -0.18f), "ToWorld");
-        //}
-        //else
-        //{
-        //    positionTray = ConvertWorldASA(new Vector3(0.39f, -0.02f, -0.15f), "ToWorld");
-        //}
-
-        
-        positionTray = ConvertWorldASA(new Vector3(1.03f, -0.03f, -0.18f), "ToWorld");
-
-        tray.transform.position = positionTray;
-        tray.transform.rotation = spatialAnchor.transform.rotation;
-
-        return positionTray;
+        UpdateTargetVisibility(positionTargetWorld);
     }
 
     private UpdateStateResponse RequestHelpCallback(UpdateStateRequest request)
@@ -220,55 +161,21 @@ public class HelpRequestHandler : MonoBehaviour
         TODOMenu.SetActive(true);
         radialView.enabled = true;
 
-        //publisher.StringMessage("/debug", medicineName);
-        //publisher.StringMessage("/debug", requestID.ToString());
-
-        //publisher.StringMessage("/debug", requestID.ToString());
-
-        // Switch to help state
-        if (requestID == 0)
+        messageMenu = requestID switch
         {
-            messageMenu = $"Remote nurse requested your help.";
-        }
+            0 => "Remote nurse requested your help.",
+            1 => $"Help the robot look for {medicineName} (ID {markerID}).",
+            2 => $"Remove the expired medicine {medicineName} (ID {markerID}) and replace it.",
+            3 => $"Grasp {medicineName} (ID {markerID}) from the highlighted box and place it on the tray.",
+            _ => messageMenu // In case of an unexpected requestID, keep the current message
+        };
 
-        if (requestID == 1)
-        {
-            // Object is not detected
-            messageMenu = $"Help the robot look for {medicineName} (ID {markerID}).";
-        }
+        TODOMenu.GetComponentInChildren<ButtonConfigHelper>().MainLabelText = messageMenu;
+        TODOMenu.GetComponentInChildren<Interactable>().IsToggled = false;
 
-        else if (requestID == 2)
-        {
-            // Medicine is expired
-            messageMenu = $"Remove the expired medicine {medicineName} (ID {markerID}) and replace it.";
-        }
-
-        else if (requestID == 3)
-        {
-            // Object too big to grasp
-            // Highlight object
-            messageMenu = $"Grasp {medicineName} (ID {markerID}) from the highlighted box and place it on the tray.";
-        }
-
-        //Dialog.Open(DialogPrefab, DialogButtonType.OK, "Help requested", message, true);
-        ButtonConfigHelper buttonConfigHelper = TODOMenu.GetComponentInChildren<ButtonConfigHelper>();
-        Interactable menuInteractable = TODOMenu.GetComponentInChildren<Interactable>();
-
-        menuInteractable.IsToggled = false;
-
-        buttonConfigHelper.MainLabelText = messageMenu;
         textToSpeech.StartSpeaking("Remote operator has requested your help.");
 
-        UpdateStateResponse updateReceived = new UpdateStateResponse();
-        updateReceived.response = true;
-
-        return updateReceived;
-    }
-
-    // Call service to resume task
-    public void ResumeTask()
-    {
-        CallResumeService(0);
+        return new UpdateStateResponse { response = true };
     }
 
     public void CallResumeService(short id)
@@ -282,23 +189,20 @@ public class HelpRequestHandler : MonoBehaviour
         awaitingResponseUntilTimestamp = Time.time + 0.2f;
     }
 
-    public void UpdateTarget()
+    void Callback_Service(UpdateStateResponse response)
     {
-        CallResumeService(1);
-    }
-
-    void Callback_Service(UpdateStateResponse response){
         awaitingResponseUntilTimestamp = -1;
-
-        if (response.response) {
-
-        }
 
         TODOMenu.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    Vector3 UpdateTrayPosition()
     {
+        positionTray = ConvertWorldASA(new Vector3(1.03f, -0.03f, -0.18f), "ToWorld");
+
+        tray.transform.position = positionTray;
+        tray.transform.rotation = spatialAnchor.transform.rotation;
+
+        return positionTray;
     }
 }
